23ea8ca301bc12bca242d6db87bdcef1929898c8
Pashka Antonov <pavelcreed222@gmail.com>
2025-10-23 20:31:59 +0300

    hw-09-next

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -1,17 +1,18 @@
-
 import { dehydrate, QueryClient } from '@tanstack/react-query'
 import { fetchNoteById } from "@/lib/api/serverApi";
 import NoteDetailsClient from '@/components/NoteDetails/NoteDetails';
 import { Metadata } from 'next';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+
 type Props = {
-  params: { id: string }
-}
+  params: { id: string };
+};
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { id } = params;
   const note = await fetchNoteById(id);
-   return {
+
+  return {
     title: `Note: ${note.title} | NoteHub`,
     description: note.content.slice(0, 60),
     openGraph: {
@@ -27,13 +28,14 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function NoteDetailsPage({ params }: Props) {
   const { id } = params;
-  const queryClient = new QueryClient();
 
+  const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(id),
   });
-   const dehydratedState = dehydrate(queryClient);
+
+  const dehydratedState = dehydrate(queryClient);
 
   return (
     <TanStackProvider dehydratedState={dehydratedState}>
